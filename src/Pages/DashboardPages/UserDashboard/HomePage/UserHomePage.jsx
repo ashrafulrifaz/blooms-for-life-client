80b2e3e8f717e566eff55cb5722887b16664e260
ashrafulrifaz <developerashrafulislamrifaz@gmail.com>
2023-11-26 10:54:20 +0600

    added filter option

--- src/Pages/DashboardPages/UserDashboard/HomePage/UserHomePage.jsx
+++ src/Pages/DashboardPages/UserDashboard/HomePage/UserHomePage.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import useDonationRequests from "../../../../Hooks/useDonationRequests";
 import UserDonationCard from "./UserDonationCard";
+import { Link } from "react-router-dom";
 
 const UserHomePage = () => {
     const {data, isPending, user} = useDonationRequests()
@@ -12,7 +13,7 @@ const UserHomePage = () => {
         <div className="user_home">
             <h1 className="capitalize text-2xl font-second">Welcome {user?.displayName}. Thank you for joining BloomsForLife</h1>
             <div className="mt-8 bg-white rounded-xl p-5">
-                <h3 className="text-lg">Your recent donation</h3>
+                <h3 className="text-lg capitalize">Your recent donation requests</h3>
                 <div className="overflow-x-auto my-8 wrapper">
                     <table className="table">
                         <thead className="bg-[#D7EDFF] text-[#39A7FF] uppercase text-[13px]" style={{borderRadius: '50px'}}>
@@ -28,11 +29,19 @@ const UserHomePage = () => {
                         </thead>
                         <tbody>
                             {
-                                data && data.map((item, idx) => <UserDonationCard key={idx} item={item} currentStatus={currentStatus} setCurrentStatus={setCurrentStatus}></UserDonationCard>)
+                                data && data.map((item, idx) => <UserDonationCard key={idx} item={item} currentStatus={currentStatus} setCurrentStatus={setCurrentStatus}></UserDonationCard>).slice(0, 3)
                             }
                         </tbody>
                     </table>
                 </div>
+                {
+                    data.length > 3 && 
+                    <div className="text-center">
+                        <Link to="/dashboard/my-donation-requests">
+                            <button>View My All Requests</button>
+                        </Link>
+                    </div>
+                }
             </div>
         </div>
     );
--- src/Pages/DashboardPages/UserDashboard/MyDonationRequests/MyDonationRequests.jsx
+++ src/Pages/DashboardPages/UserDashboard/MyDonationRequests/MyDonationRequests.jsx
@@ -1,8 +1,82 @@
+import { useEffect, useState } from "react";
+import useDonationRequests from "../../../../Hooks/useDonationRequests";
+import UserDonationCard from "../HomePage/UserDonationCard";
 
 const MyDonationRequests = () => {
+    const {data, isPending, user} = useDonationRequests()
+    const [currentStatus, setCurrentStatus] = useState(null)
+    const [currentData, setCurrentData] = useState(data || [])
+    console.log(currentData);
+
+    useEffect(() => {
+        setCurrentData(data)
+    }, [data]) 
+
+    // filtering data
+    const pendingData = data?.filter(item => item.status === 'pending')
+    const inprogressData = data?.filter(item => item.status === 'inprogress')
+    const doneData = data?.filter(item => item.status === 'done')
+    const canceledData = data?.filter(item => item.status === 'canceled')
+    
+
+    const handleFiltering = e => {
+        const filterValue = e.target.value;
+        setCurrentStatus(filterValue)
+        if(filterValue === 'all'){
+            setCurrentData(data)
+        } else if(filterValue === 'pending') {
+            setCurrentData(pendingData)
+        } else if(filterValue === 'inprogress') {
+            setCurrentData(inprogressData)
+        } else if(filterValue === 'done') {
+            setCurrentData(doneData)
+        } else if(filterValue === 'canceled') {
+            setCurrentData(canceledData)
+        } else {
+            setCurrentData(data)
+        }
+    }
+
     return (
-        <div>
-            
+        <div className="user_home">
+            <h1 className="capitalize text-2xl font-second">Welcome {user?.displayName}. Thank you for joining BloomsForLife</h1>
+            <div className="mt-8 bg-white rounded-xl p-5">
+                <div className="flex items-center justify-between">
+                    <h3 className="text-lg capitalize">Your all donation requests</h3>
+                    <select onChange={handleFiltering} name="filter" className="capitalize border border-slate-300 rounded-lg focus:outline-none py-1 px-2">
+                        <option value="all">all</option>
+                        <option value="pending">pending</option>
+                        <option value="inprogress">inprogress</option>
+                        <option value="done">done</option>
+                        <option value="canceled">canceled</option>
+                    </select>
+                </div>
+                <div className="overflow-x-auto my-8 wrapper">
+                    <table className="table">
+                        <thead className="bg-[#D7EDFF] text-[#39A7FF] uppercase text-[13px]" style={{borderRadius: '50px'}}>
+                            <tr>
+                                <th>recipient name</th>
+                                <th>recipient location</th>
+                                <th>Date & Time</th>
+                                { currentStatus === 'inprogress' && <th>donor info</th> }
+                                <th>Status</th>
+                                { currentStatus === 'inprogress' && <th>Update Status</th> }
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                currentData?.length > 0 &&
+                                currentData && currentData?.map((item, idx) => <UserDonationCard key={idx} item={item} currentStatus={currentStatus} setCurrentStatus={setCurrentStatus}></UserDonationCard>)
+                            }
+                        </tbody>
+                    </table>
+                    {
+                        currentData?.length === 0 &&
+                        <p className="text-center py-5 text-xl capitalize font-second">no {currentStatus} data found</p>
+                    }
+                </div>
+            </div>
         </div>
     );
 };
